69c7095f30ddf134a9d055fda3faa7b33c1d6c97
Victoria Olanipekun <queentoria1@gmail.com>
2021-07-01 11:30:07 +0100

    seeding database

---
+++ db/data/drinks.js
@@ -0,0 +1,268 @@
+export default [
+  {
+    drink: ' Vietnamese iced coffee, cà phê đá',
+    type: 'Coffee ',
+    country: 'Vietnam ',
+    description: ' Made using medium to coarse ground dark roast Vietnamese-grown coffee with a small metal Vietnamese drip filter (phin cà phê). This finished cup of hot coffee is then quickly poured into a glass full of ice making the finished Vietnamese iced coffee.' ,
+    image: ' https://quangtanhoa.com/wp-content/uploads/2018/01/ca-phe-den-da.jpg' ,
+    funFact: ' '
+  },
+  {
+    drink: ' Turkish coffee, Türk kahvesi',
+    type: 'Coffee ',
+    country: 'Turkey ',
+    description: ' Very finely ground coffee brewed by boiling.' ,
+    image: ' https://www.thespruceeats.com/thmb/z0dysGil3bQVHw7sgWFmACfC_pc=/4000x2667/filters:fill(auto,1)/turkish-coffee-recipe-2355497-hero-02-b14bd83fe6114322af9d8040801e8391.jpg' ,
+    funFact: 'First appearing in the Ottoman Empire, under the strictest interpretations of the Quran the strong coffee was considered a drug and its consumption was forbidden '
+  },
+  {
+    drink: ' Cuban espresso',
+    type: 'Coffee ',
+    country: 'Cuba ',
+    description: 'An espresso shot,  sweetened  with natural brown sugar which has been whipped with the first and strongest drops of espresso. ' ,
+    image: 'https://331mrnu3ylm2k3db3s1xd1hg-wpengine.netdna-ssl.com/wp-content/uploads/2021/04/egg_coffee.jpg ' ,
+    funFact: ' '
+  },
+  {
+    drink: 'Scandinavian Egg Coffee ',
+    type: 'Coffee ',
+    country: 'Norway/Sweden ',
+    description: ' Coffee grounds are mixed with raw egg before being added to boiling water.' ,
+    image: ' https://331mrnu3ylm2k3db3s1xd1hg-wpengine.netdna-ssl.com/wp-content/uploads/2021/04/egg_coffee.jpg' ,
+    funFact: 'It\'s said that Scandinavian egg coffee artisans first used eggs to temper the bitterness of poor-tasting coffee, and clarify the final brew. '
+  },
+  {
+    drink: ' Cafe de Olla ',
+    type: 'Coffee ',
+    country: 'Mexico ',
+    description: 'Contains a mix of three spices (cloves, anise, and cinnamon) and a minimally-processed sugar called piloncillo.' ,
+    image: 'https://www.maricruzavalos.com/wp-content/uploads/2020/12/cafe_de_olla_recipe.jpg ' ,
+    funFact: ' '
+  },
+  {
+    drink: ' Flat white',
+    type: 'Coffee ',
+    country: 'Australia ',
+    description: 'Espresso mixed with a small amount of steamed milk and a thin layer of microfoam.' ,
+    image: 'https://perfectdailygrind.com/wp-content/uploads/2018/11/flatwhite5.jpg ' ,
+    funFact: ' '
+  },
+  {
+    drink: 'Mazagran',
+    type: ' coffee',
+    country: 'Algeria ',
+    description: 'Cold black brew with a simple syrup and a squeeze of lemon ' ,
+    image: 'https://coffeeordie.com/wp-content/uploads/2020/05/mazagran.jpg ' ,
+    funFact: ' '
+  },
+  {
+    drink: 'Frappe ',
+    type: 'Coffee ',
+    country: 'Greece',
+    description: 'Nescafe instant coffee, sugar, and a little water are shaken or blended together until a thick foam is made. This is poured into the serving glass and ice cubes, cold water, and, optionally, evaporated milk are added to it.' ,
+    image: 'https://herbsandflour.com/wp-content/uploads/2020/08/frappe-750x1000.jpg?x92940' ,
+    funFact: 'The frappé was invented through experimentation by Dimitris Vakondios, a Nescafe representative, in 1957 in Thessaloniki.'
+  },
+  {
+    drink: 'Pharisäer',
+    type: 'Coffee',
+    country: 'Germany ',
+    description: 'made with rum and whipped cream, then mixed with sugar to taste and served with whipped cream on top. Dark roasted coffee beans and about two ounces of strong rum are often used.' ,
+    image: 'https://www.petitappetit.de/wp-content/uploads/2016/01/Pharisaeer-4.jpg' ,
+    funFact: 'It was invented for the christening of a baby girl, Johanna Theodora Katharina, on Nordstrand Island on Feb. 29, 1872.'
+  },
+  {
+    drink: 'Little Coffee, Cafezhino',
+    type: 'Coffee',
+    country: 'Brazil',
+    description: 'Cafezinho is very strong, slowly brewed coffee. It is served black, with a lot of sugar..' ,
+    image: 'http://1.bp.blogspot.com/_OfYYlOGGnDk/S3Fhb-VeFCI/AAAAAAAAA9A/H7Q88XIFYhQ/w1200-h630-p-k-no-nu/5443cafezinho.jpg' ,
+    funFact: ' '
+  },
+  {
+    drink: 'Vietnamese egg coffee, cà phê trứng',
+    type: 'Coffee',
+    country: 'Vietnam',
+    description: 'Brewed coffee with chicken egg yolk, and condensed milk. It has a similar taste and texture to tiramisu and eggnog' ,
+    image: 'https://cdn.dayphache.edu.vn/wp-content/uploads/2016/02/ca-phe-trung.jpg' ,
+    funFact: ' '
+  },
+  {
+    drink: 'Espresso',
+    type: 'Coffee',
+    country: 'Italy',
+    description: 'Pressurized steam is passed through finely ground coffee beans to create a very strong coffee with froth on top' ,
+    image: 'https://www.italianmade.com/usa/wp-content/uploads/2017/11/Caff%C3%A8.jpg' ,
+    funFact: ' '
+  },
+  {
+    drink: 'Mélange',
+    type: 'Coffee',
+    country: 'Austria',
+    description: 'Similar to a cappuccino, mélange is espresso with steamed milk, topped with a little foam.' ,
+    image: 'https://i0.wp.com/cafeflavour.com/wp-content/uploads/2017/01/beige-20553_1920.jpg?fit=1920%2C1279' ,
+    funFact: ' '
+  },
+  {
+    drink: 'Irish coffee',
+    type: 'Coffee',
+    country: 'Ireland',
+    description: 'A mix of hot coffee, Irish whiskey, brown sugar, and whipped cream. It is really more of a special dessert than anything else!' ,
+    image: ' ' ,
+    funFact: 'It was created in Ireland in 1943'
+  },
+  {
+    drink: 'Kaffeost',
+    type: 'Coffee',
+    country: 'Finland',
+    description: 'A dried cheese called juustoleipä is placed at the bottom of a wooden cup. Hot coffee is poured over and the juustoleipä absorbs the coffee like a sponge.' ,
+    image: ' ' ,
+    funFact: 'Kaffeost translates to "coffee cheese! '
+  },
+  {
+    drink: 'Café au lait',
+    type: 'Coffee',
+    country: 'France',
+    description: 'Brewed coffee mixed with steamed milk. There is no foam.' ,
+    image: 'https://yourcoffeebuzz.com/wp-content/uploads/2018/03/Cafe-au-lait-with-butter-croissant.jpg' ,
+    funFact: ' '
+  },
+  {
+    drink: 'Buna, ቡና',
+    type: 'Coffee',
+    country: 'Ethiopia',
+    description: 'Green coffee beans are gathered then roasted and brewed in a clay coffee pot called a jebena.' ,
+    image: 'https://explorepartsunknown.com/wp-content/uploads/2018/07/DSCF81651.jpg?quality=95&strip=color&w=620&h=0' ,
+    funFact: 'Ethiopia is actually known as the birthplace of coffee (which is called buna), and so it\'s pretty special there. The coffee ceremonies can go on for about three hours and happen a few times a day.'
+  },
+  {
+    drink: 'Cafe Bombon',
+    type: 'Coffee',
+    country: 'Spain',
+    description: 'Espresso mixed with sweetened condensed milk. ' ,
+    image: 'https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/exps51235_THCA153054B08_29_2b.jpg' ,
+    funFact: 'Can be traced back to Valencia in Spain. Bonbon means "confection. Modern versions often use ground coffee, but traditionally espresso is used.'
+  },
+  {
+    drink: 'Spiced Coffee, قهوة معطرة',
+    type: 'Coffee',
+    country: 'Morocco',
+    description: 'Made with a French press and freshly ground coffee beans, then mixed with a blend of spices including cinnamon, ginger, cardamom, cloves, black pepper, and nutmeg.' ,
+    image: 'https://moroccanzest.com/wp-content/uploads/2020/10/Moroccan-coffee-spicy-recipe-600x884.jpg' ,
+    funFact: 'Moroccan spiced coffee is usually made at home instead of in the street or in restaurants."'
+  },
+  {
+    drink: 'Cafe Touba',
+    type: 'Coffee',
+    country: 'Senegal',
+    description: 'Similar to drip coffee, but with a lot more spice! The coffee is flavored with Guinea pepper, called dijar, and cloves. It is a bit sweet but is generally actually spicy.' ,
+    image: 'https://thecoffeeguru.net/wp-content/uploads/2020/06/Cafe%CC%81-Touba-Senegal.jpg' ,
+    funFact: 'Cafe touba is thought to have aphrodisiac qualities.'
+  },
+  {
+    drink: 'Masala Chai, मसाला चाय',
+    type: 'Tea',
+    country: 'India',
+    description: 'Black tea mixed with herbs and spices. Traditionally prepared as a decoction of green cardamom pods, cinnamon sticks, ground cloves, ground ginger, and black peppercorn together with black tea leaves' ,
+    image: 'https://www.zliving.com/wp-content/uploads/2018/06/masalachairecipe.jpg' ,
+    funFact: ' '
+  },
+  {
+    drink: 'Taiwanese bubble tea, 珍珠奶茶',
+    type: 'Tea',
+    country: 'Taiwan',
+    description: 'Milk tea accompanied by chewy tapioca balls' ,
+    image: 'https://theforkedspoon.com/wp-content/uploads/2019/05/Thai-Iced-Tea-Boba-6-500x500.jpg' ,
+    funFact: 'There is a common misconception that the name "bubble tea" refers to the appearance of the pearls (or boba), but it originates from the appearance of bubbles in the milk and tea blend after it is removed from the shaking machine.'
+  },
+  {
+    drink: 'Po Cha, བོད་ཇ་ ',
+    type: 'Tea',
+    country: 'Tibet and Himalayan regions of Southeast Asia',
+    description: 'Black-tea leaves from the Pemagul region are boiled and left to steep for several hours. After, fresh yak butter and salt are added to the strained tea, which is then shaken or churned, creating a thick tea.' ,
+    image: 'https://s-media-cache-ak0.pinimg.com/736x/20/58/98/205898a905024a8b0785c2feb6afcb79.jpg' ,
+    funFact: 'This tea dates back to the 7th century. It is traditionally enjoyed from bowls rather than mugs'
+  },
+    
+  {
+    drink: 'Breakfast Tea',
+    type: 'Tea',
+    country: 'UK and Ireland',
+    description: 'A traditional blend of black teas originating from Assam, Ceylon and Kenya. Stronger than tea you would have during later times in the day.' ,
+    image: 'https://static.onecms.io/wp-content/uploads/sites/19/2018/02/13/tea-cup-2000.jpg' ,
+    funFact: 'Despite strong evidence for a Scottish origin, the blend became popular as an "English" breakfast tea.'
+  },
+  {
+    drink: 'Yerba Mate',
+    type: 'Tea',
+    country: 'Argentina, Paraguay, Uruguay, and Southern South-America in general',
+    description: 'Rich in antioxidants, the coffee-like tea is also said to possess several health benefits. It is brewed in a hollowed-out gourd and drunk through a metal straw called a bombilla.' ,
+    image: 'https://blog.amigofoods.com/wp-content/uploads/2011/05/yerba-mate-products.jpg' ,
+    funFact: 'It’s almost impossible to walk down the street in Argentina or Paraguay without seeing hordes of people sipping mate. Whether hot or iced, mate is regarded as a social facilitant, as single servings are often passed between friends and family during celebrations.'
+  },
+  {
+    drink: 'Brew, Zavarka Заварка',
+    type: 'Tea',
+    country: 'Russia',
+    description: 'A loose-leaf tea concentrate brewed in a small metal container called a Samovar. In this vessel, a very strong, black tea is brewed and then served. Take an inch or less of this powerful concoction then dilute with boiling water as desired.' ,
+    image: 'https://sites.google.com/site/crimepunishmentfood/_/rsrc/1432254842348/nastasya/Russian-tea.jpg' ,
+    funFact: 'Russians typically drink it black, but hosts will offer milk and sugar, as well as an accompanying snack. Serving zavarka without cookies, crackers or some other munchable is to serve it "naked" and is considered wildly rude.'
+  },
+  {
+    drink: 'Pink tea, Gulabi Chai گلابی چائے',
+    type: 'Tea',
+    country: 'Pakistan',
+    description: 'Noon Chai is a special blend of tea that includes a mix of pistachios, almonds, salt, milk, and spices like cardamom, cinnamon, and star anise. The signature pink color is enhanced with a bit of baking soda.' ,
+    image: 'https://www.mirchitales.com/wp-content/uploads/2015/10/KashmiriChai-3-1080x720.jpg' ,
+    funFact: 'Served on special occasions, Noon Chai is typically enjoyed with pastries like sheermaal, kandir tchot, bakarkhani, and kulcha. '
+  },
+  {
+    drink: 'Persian Chai, چاییi',
+    type: 'Tea',
+    country: 'Iran',
+    description: 'Strong tea is served accompanied by a bright yellow rock candy called nabat. Rather than mixing in sugar to counteract the bitterness, you should place a sugar cube between your front teeth and suck the strong brew through it. ' ,
+    image: 'http://persianfoodtours.com/wp-content/uploads/2018/11/IMG_8806.jpg' ,
+    funFact: ' '
+  },
+  {
+    drink: 'Matcha, 抹茶',
+    type: 'Tea',
+    country: 'Japan',
+    description: 'Matcha is finely ground powder of specially grown and processed green tea leaves. The mixture of water and tea powder is whisked using a special bamboo whisk. served with a small sweet to counteract the bitterness.' ,
+    image: 'https://www.peacewiththewild.co.uk/wp-content/uploads/2020/10/teapigs-matcha-30g-cups.jpg' ,
+    funFact: 'In modern times, matcha is also used to flavor and dye foods, such as mochi and soba noodles, green tea ice cream, matcha lattes and a variety of Japanese confectionery.'
+  },
+  {
+    drink: 'Pu Erh, 普洱茶',
+    type: 'Tea',
+    country: 'China',
+    description: 'Pu Erh is a variety of fermented tea that stems from the Yunnan province of China. strong, rich and earthy taste' ,
+    image: 'https://blog.tutorming.com/hubfs/pu_er_tea.jpg' ,
+    funFact: 'Bought in blocks used when entertaining special guests. It is seen as magical tea and does take years to form. Pu\'erh is the only tea that is measured in years - the older the better.'
+  },
+  {
+    drink: 'Omija-cha, 오미자 차',
+    type: 'Tea',
+    country: 'Korea',
+    description: 'Made by boiling dried magnolia berries in water on low heat, then adding honey.' ,
+    image: 'https://images-na.ssl-images-amazon.com/images/I/41HM1ySXLFL.jpg' ,
+    funFact: 'Omija means "five flavors", which are sweetness, sourness, bitterness, saltiness, and pungency'
+  },
+  {
+    drink: 'Maghrebi mint tea, الشاي المغربي بالنعناع',
+    type: 'Tea',
+    country: 'Maghreb region, spread across Morocco, Algeria, Tunisia, Libya, and Mauritania',
+    description: 'Gunpowder green tea infused with spearmint and herbs such as sage, woodworm, lemon verbena, wild thyme and wild geranium. Sweetened with sugar.' ,
+    image: 'https://www.thespruceeats.com/thmb/U-1j92gq_j-5AX9nvSxDDN8jUIM=/1830x1830/smart/filters:no_upscale()/tea-getty-2097-x-1830-56a645d63df78cf7728c31d9.jpg' ,
+    funFact: 'Often involves an intricate brewing process and fairly formal service, called Atai.'
+  },
+  {
+    drink: 'Teh Tarik',
+    type: 'Tea',
+    country: 'Malaysia',
+    description: 'It is made from a strong brew of black tea blended with condensed milk. The mixture is poured back and forth repeatedly between two vessels from a height, giving it a thick frothy top. This process cools the tea to optimal drinking temperatures, thoroughly mixes the tea with the condensed milk, and improves its flavour.' ,
+    image: 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/8252F1F2-D924-401F-B167-0F4ACEBEB819/Derivates/84404ac4-d061-4d45-938e-11c2de0218e6.jpg' ,
+    funFact: 'Its name is derived from the pouring process of "pulling" the drink during preparation. '
+  },
+    
+]

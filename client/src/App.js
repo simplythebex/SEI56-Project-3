3e7e1d629df0eb3e9f74d034cb4fd1019921a3ae
simplythebex <becca.jones@live.co.uk>
2021-07-02 12:58:02 +0100

    Updated content for homepage in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
+// import axios from 'axios'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
+import Figure from 'react-bootstrap/Figure'
 
 const App = () => {
-  const [drinks, setDrinks] = useState([])
+  // const [drinks, setDrinks] = useState([])
 
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get('/api/drinks')
-      setDrinks(data)
-    }
-    getData()
-  }, [])
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const { data } = await axios.get('/api/drinks')
+  //     setDrinks(data)
+  //   }
+  //   getData()
+  // }, [])
 
   return (
     <>
-      {drinks.map(drink => <h1 key={drink._id}> {drink.drink} </h1>)}
-      <h1>Hello world</h1>
+      {/* {drinks.map(drink => <h1 key={drink._id}> {drink.drink} </h1>)} */}
+      <Container fluid className="hero">
+        <Navbar expand="lg">
+          <Container>
+            <div className="navbar-left">
+              <Navbar.Brand href="#home">Heiss</Navbar.Brand>
+            </div>
+            <div className="navbar-right">
+              <Navbar.Brand href="#index">Our Coffees</Navbar.Brand>
+              <Navbar.Brand href="#index">Our Teas</Navbar.Brand>
+              <Navbar.Brand href="#index">Order Online</Navbar.Brand>
+              <Navbar.Brand href="#index">Heiss Room</Navbar.Brand>
+              <Navbar.Brand href="#index">About us</Navbar.Brand>
+              <Button variant="outline-light">Sign up</Button>{' '}
+              <Button variant="outline-light">Login</Button>{' '}
+            </div>
+          </Container>
+        </Navbar>
+        <Container>
+          <h2>Hot Drinks Around The World</h2>
+          <p>Your favourite tea and coffee.</p>
+          <Container>
+            <Button variant="outline-light">Suggest Hot Drinks</Button>{' '}
+            <Button variant="warning">Shop Hot Drinks</Button>{' '}
+          </Container>
+        </Container>
+      </Container>
+      <Figure>
+        <Figure.Image
+          alt="zig-zag"
+          src="./styles/images/style.png" 
+        /> 
+      </Figure>
+      
     </>
   )
 }

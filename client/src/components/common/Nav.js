ed193d563b1d31951a1c718d00394354dcbac53b
Victoria Olanipekun <queentoria1@gmail.com>
2021-07-05 10:27:05 +0100

    home frontend

--- client/src/components/common/Nav.js
+++ client/src/components/common/Nav.js
@@ -1,26 +1,39 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
+// import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
 // import Figure from 'react-bootstrap/Figure'
 
 const Nav = () => {
   return (
     <Navbar expand="lg">
-      <Container>
-        <div className="navbar-left">
-          <Navbar.Brand href="#home">Heiss</Navbar.Brand>
-        </div>
-        <div className="navbar-right">
-          <Navbar.Brand href="#index">Our Coffees</Navbar.Brand>
-          <Navbar.Brand href="#index">Our Teas</Navbar.Brand>
-          <Navbar.Brand href="#index">Order Online</Navbar.Brand>
-          <Navbar.Brand href="#index">Heiss Room</Navbar.Brand>
-          <Navbar.Brand href="#index">About us</Navbar.Brand>
-          <Button variant="outline-light">Sign up</Button>{' '}
-          <Button variant="outline-light">Login</Button>{' '}
-        </div>
-      </Container>
+      <div className="main">
+        <Row>
+          <Col>
+            <div className="navbar-left">
+              <Navbar.Brand href="#home">Heiss</Navbar.Brand>
+            </div>
+          </Col>
+          <Col className="column">
+            <div className="navbar-right">
+              <Navbar.Brand href="#index">Our coffees</Navbar.Brand>
+              <Navbar.Brand href="#index">Our teas</Navbar.Brand>
+              <Navbar.Brand href="#index">Order online</Navbar.Brand>
+              <Navbar.Brand href="#index">Heiss room</Navbar.Brand>
+              <Navbar.Brand href="#index">About us</Navbar.Brand>
+            </div>
+          </Col>
+          <Col>
+            <div className="navbar-right-right">
+              <Button variant="outline-light register">Sign up</Button>{' '}
+              <Button variant="outline-light login">Login</Button>{' '}
+            </div>
+          </Col>
+        </Row>
+      </div>
     </Navbar>
   )
 }

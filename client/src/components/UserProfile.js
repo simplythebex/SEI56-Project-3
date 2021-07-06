7c2f64a1e255288804029e5e47e17cef54067d29
Ole Nascimento <olenascimento@gmail.com>
2021-07-06 15:38:58 +0200

    oops

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -51,34 +51,36 @@ const UserProfile = () => {
       <div className="nav-container-pages">
         <Nav />
       </div>
-      <Breadcrumb className="breadcrump">
-        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-        <Breadcrumb.Item active>Profile</Breadcrumb.Item>
-      </Breadcrumb>
-      <h3>Your suggestions</h3>
-      <Container className="createdItems">
-        <Col>
-          {userInfo.map(info => {
-            return (
-              <>
-                <Container className="suggestions">
-                  <Card style={{ width: '18rem' }}>
-                    <Card.Body>
-                      <Card.Title><p key={info.drink}>{info.drink}</p></Card.Title>
-                      <Card.Subtitle className="mb-2 text-muted"><p key={info.type}>{info.type}</p></Card.Subtitle>
-                      <Card.Text>
+      <Container className="main-container">
+        <Breadcrumb className="breadcrump">
+          <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+          <Breadcrumb.Item active>Profile</Breadcrumb.Item>
+        </Breadcrumb>
+        <h3>Your suggestions</h3>
+        <Container className="createdItems">
+          <Col>
+            {userInfo.map(info => {
+              return (
+                <>
+                  <Container className="suggestions">
+                    <Card style={{ width: '18rem' }}>
+                      <Card.Body>
+                        <Card.Title><p key={info.drink}>{info.drink}</p></Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted"><p key={info.type}>{info.type}</p></Card.Subtitle>
+                        <Card.Text>
                       
-                      </Card.Text>
-                      <Button className="outline-light" to="/suggest"variant="warning">Edit</Button>
-                      <Button variant="warning">Delete</Button>
-                    </Card.Body>
-                  </Card>
-                </Container>
-              </>
-            )
-          })}
-        </Col>
+                        </Card.Text>
+                        <Button className="outline-light" to="/suggest"variant="warning">Edit</Button>
+                        <Button variant="warning">Delete</Button>
+                      </Card.Body>
+                    </Card>
+                  </Container>
+                </>
+              )
+            })}
+          </Col>
 
+        </Container>
       </Container>
     </>
   )

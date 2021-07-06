5dad41090978812b6ad16ea040573318fcfe9094
Ole Nascimento <olenascimento@gmail.com>
2021-07-06 10:44:53 +0200

    userprofile added showing username

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,33 +1,35 @@
 import axios from 'axios'
-import React, { useEffect } from 'react' 
+import React, { useEffect, useState } from 'react' 
 import { useParams } from 'react-router-dom'
 import { getTokenFromLocalStorage } from './helpers/auth'
+import Nav from './common/Nav.js'
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import Container from 'react-bootstrap/esm/Container'
+import Col from 'react-bootstrap/esm/Col'
 
 const UserProfile = () => {
+  const [userInfo,  setUserInfo] = useState([])
+
   const { id } = useParams()
 
   
   useEffect(() => {
     const getData = async () => {
       try {
-        const res = await axios.get(`/api/profile/${id}`, 
+        const { data } = await axios.get(`/api/profile/${id}`, 
           { 
             headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
           }
         )
-        console.log('res', res)
+        setUserInfo(data)
       } catch (err) {
         console.log('err', err)
       }
-      
     }
-
     getData()
-
   }, [id])
-
-
-
+  console.log('userInfo', userInfo)
+  
 
 
 
@@ -35,9 +37,18 @@ const UserProfile = () => {
 
   return (
     <>
-      
-      <h1>User Profile</h1>
+      <Nav />
+      <Breadcrumb>
+        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+        <Breadcrumb.Item active>Profile</Breadcrumb.Item>
+      </Breadcrumb>
+      <h3>Welcome: {userInfo.username}</h3>
+      <Container className="createdItems">
+        <Col>
+          {<h3>Drink: {userInfo.createdDrinks[1].drink}</h3>}
+        </Col>
 
+      </Container>
     </>
   )
 

2d11025e20ed644a45615d46dafd8be8b40a92b9
Ole Nascimento <olenascimento@gmail.com>
2021-07-08 10:44:57 +0200

    added seed file for suggestions from user to be shown on user profile

--- db/data/suggestedDrinks.js
+++ db/data/suggestedDrinks.js
@@ -1,5 +1,88 @@
 export default [
   {
+    drink: 'British late',
+    type: 'coffee',
+    country: 'sweden',
+    description: 'test test',
+    image: 'https://www.zliving.com/wp-content/uploads/2018/06/masalachairecipe.jpg',
+    funFact: 'test test',
+    origin: 'Europe'
+  },
+
+  {
+    drink: 'Italian late',
+    type: 'tea',
+    country: 'Norway',
+    description: 'Iciy',
+    image: 'https://www.zliving.com/wp-content/uploads/2018/06/masalachairecipe.jpg',
+    funFact: 'test test',
+    origin: 'Europe'
+  },
+  {
+    drink: 'German late',
+    type: 'coffee',
+    country: 'Germany',
+    description: 'test test',
+    image: 'https://www.zliving.com/wp-content/uploads/2018/06/masalachairecipe.jpg',
+    funFact: 'test test',
+    origin: 'Europe'
+  },
+  {
+    drink: 'French late',
+    type: 'coffee',
+    country: 'sweden',
+    description: 'test test',
+    image: 'https://www.zliving.com/wp-content/uploads/2018/06/masalachairecipe.jpg',
+    funFact: 'test test',
+    origin: 'Europe'
+  },
+  {
+    drink: 'American late',
+    type: 'coffee',
+    country: 'sweden',
+    description: 'test test',
+    image: 'https://www.zliving.com/wp-content/uploads/2018/06/masalachairecipe.jpg',
+    funFact: 'test test',
+    origin: 'Europe'
+  },
+  {
+    drink: 'Brazilian late',
+    type: 'coffee',
+    country: 'sweden',
+    description: 'test test',
+    image: 'https://www.zliving.com/wp-content/uploads/2018/06/masalachairecipe.jpg',
+    funFact: 'test test',
+    origin: 'Europe'
+  },
+  {
+    drink: 'South African late',
+    type: 'coffee',
+    country: 'sweden',
+    description: 'test test',
+    image: 'https://www.zliving.com/wp-content/uploads/2018/06/masalachairecipe.jpg',
+    funFact: 'test test',
+    origin: 'Europe'
+  },
+  {
+    drink: 'Thai late',
+    type: 'coffee',
+    country: 'sweden',
+    description: 'test test',
+    image: 'https://www.zliving.com/wp-content/uploads/2018/06/masalachairecipe.jpg',
+    funFact: 'test test',
+    origin: 'Europe'
+  },
+  {
+    drink: 'Belgian late',
+    type: 'coffee',
+    country: 'sweden',
+    description: 'test test',
+    image: 'https://www.zliving.com/wp-content/uploads/2018/06/masalachairecipe.jpg',
+    funFact: 'test test',
+    origin: 'Europe'
+  },
+  {
+    drink: 'Australian late',
     type: 'coffee',
     country: 'sweden',
     description: 'test test',
@@ -7,5 +90,4 @@ export default [
     funFact: 'test test',
     origin: 'Europe'
   }
-
 ]
